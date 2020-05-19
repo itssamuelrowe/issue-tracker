d8bf9aae3cfe90a24086f30c332fd03879c0ffa9
Samuel Rowe <samuelrowe1999@gmail.com>
2020-05-19 10:53:08 +0530

    Reorganized the source files.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const mongodb = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const issue = require('./issue.js');
 
 const app = express();
 app.use(express.static('static'));
@@ -32,40 +33,6 @@ app.get('/api/issues', (request, response) => {
 		response.status(500).json({ message: error });
 	})
 });
-
-const validIssueStatus = {
-	New: true,
-	Open: true,
-	Assigned: true,
-	Fixed: true,
-	Verified: true,
-	Closed: true,
-};
-
-const issueFieldType = {
-	status: 'required',
-	owner: 'required',
-	effort: 'optional',
-	created: 'required',
-	completionDate: 'optional',
-	title: 'required'
-};
-
-function validateIssue(issue) {
-	for (const field in issueFieldType) {
-		const type = issueFieldType[field];
-    	if (!type) {
-     	 	delete issue[field];
-		}
-		else if (type === 'required' && !issue[field]) {
-      		return `${field} is required.`;
-    	}
-  	}
-  	if (!validIssueStatus[issue.status]) {
-    	return `${issue.status} is not a valid status.`;
-    }
-  	return null;
-}
 
 app.post('/api/issues', (request, response) => {
 	const newIssue = request.body;
